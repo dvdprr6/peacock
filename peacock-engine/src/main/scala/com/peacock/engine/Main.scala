1380c4bedbd2f226191f9bba31ef6e67a376007b
david <dvdprr6@gmail.com>
2022-06-22 20:54:13 -0400

    first commit

---
+++ peacock-engine/src/main/scala/com/peacock/engine/Main.scala
@@ -0,0 +1,23 @@
+package com.peacock.engine
+import zhttp.http._
+import zhttp.http.middleware.HttpMiddleware
+import zhttp.service.Server
+import zio.clock.Clock
+import zio.console.Console
+import zio.{ExitCode, URIO, ZIO}
+
+import java.io.IOException
+
+object Main extends zio.App {
+  private val PEACOCK_ENGINE ="peacock-engine"
+
+  private val app = Http.collectZIO[Request]{
+    case Method.GET -> !! / PEACOCK_ENGINE / "fruits" => ZIO.succeed(Response.text("Apple"))
+  }
+
+  private val middlewares: HttpMiddleware[Console with Clock, IOException] =
+    Middleware.debug ++ Middleware.addHeader("X-Environment", "Dev")
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    Server.start(8090, app @@ middlewares).exitCode
+}
---
+++ peacock-engine/src/main/scala/com/peacock/engine/services/HttpClientService.scala
@@ -0,0 +1,25 @@
+package com.peacock.engine.services
+
+import zhttp.http.{Headers, Method, Response}
+import zhttp.service.{ChannelFactory, Client, EventLoopGroup}
+import zio.{Has, RIO, RLayer, ZEnv, ZIO, ZLayer}
+
+object HttpClientService {
+  type HttpClientServiceEnv = Has[HttpClientService.Service]
+
+  trait Service{
+    def getRequest(apiKey: String, url: String): RIO[ZEnv, Response]
+  }
+
+  def getRequest(apiKey: String, url: String): RIO[ZEnv with HttpClientServiceEnv, Response] =
+    ZIO.accessM(_.get[HttpClientService.Service].getRequest(apiKey, url))
+
+  lazy val live: RLayer[ZEnv, HttpClientServiceEnv] =
+    ZLayer.succeed((apiKey, url) => {
+      val headers = Headers.apply("apikey", apikey)
+
+      for{
+        response <- Client.request(url = url, method = Method.GET, headers = headers).provideCustomLayer(ChannelFactory.auto ++ EventLoopGroup.auto())
+      } yield response
+    })
+}
